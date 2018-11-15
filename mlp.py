39b1d85d471e70a19e4c8b2bdf36b1545cd470ae
Philippe Marcotte <philippe.marcotte@polymtl.ca>
2018-11-14 22:31:29 -0500

    created mlp model and training on fashionmnist, currently trying to use randomsearch for hyperparams optimization

---
+++ mlp.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+
+# Neural Network Model (1 hidden layer)
+class MLP(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super().__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size, num_classes)
+
+    def forward(self, x):
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc2(out)
+        return out
