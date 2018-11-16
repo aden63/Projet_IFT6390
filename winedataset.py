0da621aee489210ef75844421d38e199d2e7ccee
Philippe Marcotte <philippe.marcotte@polymtl.ca>
2018-11-16 15:23:30 -0500

    class to use the wine dataset with pytorch

---
+++ winedataset.py
@@ -0,0 +1,28 @@
+from torch.utils.data import Dataset
+import os
+import pandas as pd
+import torch
+import numpy as np
+
+class WineDataset(Dataset):
+    def __init__(self, root, wine_type, n_classes=10):
+        self.root = os.path.expanduser(root)
+        self.wine_type = wine_type
+
+        self.data = pd.read_csv(os.path.join(self.root, "winequality-{}.csv".format(self.wine_type)),
+                                      sep=";",
+                                      header=1)
+        self.data = torch.Tensor(self.data.values)
+
+    def __getitem__(self, index):
+        X = self.data[index, :-1]
+        y = self.data[index, -1].type(torch.LongTensor)
+        if y < 6:
+            y -= y
+        elif y >= 6:
+            y -= y - 1
+
+        return X, y
+
+    def __len__(self):
+        return self.data.shape[0]
